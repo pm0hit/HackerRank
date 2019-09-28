ace4b8899a4291f816bd4ee5a5a63ada410bea6e
Mohit Patel <37521593+pm0hit@users.noreply.github.com>
2019-09-28 14:53:35 -0400

    Create Java Loops I.java

---
+++ Java Loops I.java
@@ -0,0 +1,52 @@
+/* Objective
+In this challenge, we're going to use loops to help us do some simple math.
+
+Task
+Given an integer, , print its first  multiples. Each multiple  (where ) should be printed on a new line in the form: N x i = result.
+
+Input Format
+
+A single integer, .
+
+Constraints
+
+Output Format
+
+Print  lines of output; each line  (where ) contains the  of  in the form:
+N x i = result.
+*/
+
+
+//Code
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+
+        if( N>=2 && N<=20)
+        {
+            for(int i=1; i<11; i++)
+            {
+                int M= i*N;
+                System.out.println(N + " x "+i+" = "+M);
+            }
+        }
+    }
+}
